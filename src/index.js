663cb789820ff3d7cc60631f2441d7f27315ebf5
Peppej1086 <peppej1086@live.it>
2024-07-31 11:25:17 +0200

    Strict Mode deleted

--- src/index.js
+++ src/index.js
@@ -7,7 +7,5 @@ import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
